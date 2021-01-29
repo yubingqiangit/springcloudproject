8b4222287bcaeb05e9224c90f9d20419ff9447e5
kd_yubingqian <15131872726@163.com>
2021-01-29 15:04:40 +0800

    update code

--- nacos-order/src/main/java/com/yu/NaocsOrderClient.java
+++ nacos-order/src/main/java/com/yu/NaocsOrderClient.java
@@ -1,9 +1,14 @@
 package com.yu;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO 
@@ -18,4 +23,6 @@ public class NaocsOrderClient {
     public static void main(String[] args) {
         SpringApplication.run(NaocsOrderClient.class,args);
     }
+
+
 }
--- nacos-order/src/main/java/com/yu/controller/DemoController.java
+++ nacos-order/src/main/java/com/yu/controller/DemoController.java
@@ -5,6 +5,10 @@ package com.yu.controller;/**
  *
  */
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.yu.feign.Feign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +21,33 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/1/25 16:47 
  */
 @RestController
+
 public class DemoController {
 
     @Autowired
     private Feign demoFeignService;
 
+    @NacosValue(value = "${infor:bbbbb}", autoRefreshed = true)
+    private String info;
+
+    @GetMapping(value = "config")
+    public String config() {
+        System.out.println("获取rocketmq_path" + info
+        );
+        return info;
+    }
+
+
+    @SentinelResource(value = "hello", blockHandler = "blockHandlerHello")
     @GetMapping("/test")
     public String test(String name) {
         System.out.println("前段请求");
         String demo = demoFeignService.demo(name);
         System.out.println("接口返回参数：" + demo);
         return demoFeignService.demo(name);
+    }
+
+    public String blockHandlerHello(BlockException e) {
+        return "限流了";
     }
 }
